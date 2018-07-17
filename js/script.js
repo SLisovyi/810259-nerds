11b7f9d7c793cdd489287d16c2ace8c49a955a75
SSliS <39931231+SSliS@users.noreply.github.com>
2018-07-17 07:52:05 -0600

    nerds almost done

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var link = document.querySelector(".button-write-us"); // в переменную link вписали ксс класс .button-write-us
+
+var popup = document.querySelector(".modal-contact");
+var close = document.querySelector(".modal-close");
+
+var login = popup.querySelector("[name=login]");
+
+link.addEventListener("click", function (evt) {   // добавили link действие нажатия мыши
+	evt.preventDefault();						// отменили действие по умолчанию
+	console.log("Click on write us buton");
+	popup.classList.remove("visually-hidden"); 			// добавили класс
+	login.focus(); 								// ставим курсор в поле ввода после открытия попапа
+});
+
+close.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("visually-hidden");
+});
+
